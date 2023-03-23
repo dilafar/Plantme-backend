82a46539b6c8acdc5e67f7ecdcb65ce2a8007141
Isuru Pathum Herath <77333662+isurupathumherath@users.noreply.github.com>
2023-03-24 02:00:16 +0530

    test with output

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import CategoriesScreen from './screens/CategoriesScreen';
+// import CategoriesScreen from './screens/CategoriesScreen';
+
+import HelloWorld from './screens/inquiry-screens/hello-world';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,13 +14,9 @@ export default function App() {
     <StatusBar style='dark'/>
     <NavigationContainer>
          <Stack.Navigator>
-             <Stack.Screen name="Home" component={CategoriesScreen} />
+             <Stack.Screen name="Home" component={HelloWorld} />
          </Stack.Navigator>
     </NavigationContainer>
- 
-      
-    
-    
     </>
       
   );
