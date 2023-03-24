7d0976e73de67c9bb567093e3e5e0454ceef4c23
samuditha97 <samudithahasantha12@gmail.com>
2023-03-24 11:44:21 +0530

    created delivery submit form

--- App.js
+++ App.js
@@ -3,24 +3,26 @@ import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CategoriesScreen from './screens/CategoriesScreen';
+import DeliveryForm from "./components/Delivery/deliveryDataSubmit";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-    <StatusBar style='dark'/>
-    <NavigationContainer>
-         <Stack.Navigator>
-             <Stack.Screen name="Home" component={CategoriesScreen} />
-         </Stack.Navigator>
-    </NavigationContainer>
- 
-      
-    
-    
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="DeliveryForm"
+            component={DeliveryForm}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
-      
   );
 }
 
